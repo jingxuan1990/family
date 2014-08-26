ad301ed9a10a6a389908d148289b6c9855866fe2
Andy Li <jingxuan2046@126.com>
2014-08-26 19:38:54 +0800

    fixed js and add pnotify

--- js/login.js
+++ js/login.js
@@ -1,81 +1,82 @@
-$(document).ready(function(){
-	// login in the system
-	function login(){
-		var username = $("form input[type='text']").val();
-		var password = $("form input[type='password']").val();
-		if(isNotVaild(username) || isNotVaild(password)){
-			return;
-		}
-		// using ajax request for login in
-		$.ajax({
-			  type: "POST",
-			  url: 'login/user_login',
-			  data: {'username':username, 'password':password},
-			  success: function(result){
-				  if(result.status){
-					  window.location.href = 'index.php';
-				  }else{
-					  loginErrorMessage(result.message);
-				  }
-					  
-			  },
-			  error:function(result){
-				  loginErrorMessage("用户登录时发生错误！");
-			  },
-			  dataType: 'json'
-			});
-	}
-	
-	$("form button").click(function(){
-		if(vaild_Form()){
-			login(); // click the button for login in
-		}
-	});
-	
-	function vaild_Form(){
-		$(".form-control:input").each(function(index){
-			var message;
-			if($(this).attr("type") === "text"){
-				message = "用户名长度（5-16）不合法！";
-			}else{
-				message = "密码长度（5-16）不合法！";
+(function($){
+	$(function(){
+		// login in the system
+		function login(){
+			var username = $("form input[type='text']").val();
+			var password = $("form input[type='password']").val();
+			if(isNotVaild(username) || isNotVaild(password)){
+				return;
 			}
-			if(isNotVaild($(this).val())){
-				$(this).parent().removeClass('has-success');
-				$(this).parent().addClass('has-error');
-				$(this).next().text(message);
-				return false;
-			}else{
-				$(this).parent().removeClass('has-error');
-				$(this).parent().addClass('has-success');
-				$(this).next().text("");
+			// using ajax request for login in
+			$.ajax({
+				  type: "POST",
+				  url: 'login/user_login',
+				  data: {'username':username, 'password':password},
+				  success: function(result){
+					  if(result.status){
+						  window.location.href = 'index.php';
+					  }else{
+						  loginErrorMessage(result.message);
+					  }
+						  
+				  },
+				  error:function(result){
+					  loginErrorMessage("用户登录时发生错误！");
+				  },
+				  dataType: 'json'
+				});
+		}
+		
+		$("form button").click(function(){
+			if(vaild_Form()){
+				login(); // click the button for login in
 			}
 		});
-		return true;
-	}
-	
-	function isNotVaild(string){
-		var len = string.trim().length;
-		if(len <= 4 ||  len >=16){
+		
+		function vaild_Form(){
+			$(".form-control:input").each(function(index){
+				var message;
+				if($(this).attr("type") === "text"){
+					message = "用户名长度（5-16）不合法！";
+				}else{
+					message = "密码长度（5-16）不合法！";
+				}
+				if(isNotVaild($(this).val())){
+					$(this).parent().removeClass('has-success');
+					$(this).parent().addClass('has-error');
+					$(this).next().text(message);
+					return false;
+				}else{
+					$(this).parent().removeClass('has-error');
+					$(this).parent().addClass('has-success');
+					$(this).next().text("");
+				}
+			});
 			return true;
 		}
-	}
-	
-	function loginErrorMessage(message){
-		var html = $.parseHTML("<div class='alert alert-danger' role='alert'>" + message +"</div>");
-	    $("#alertTemplate").append(html);
-		setTimeout(function(){
-			$("#alertTemplate").html("");
-		}, 2000);
-	}
-
-	$("body").on("keyup", function(event){
-		if(event.keyCode === 13){
-			if(vaild_Form()){
-				login();
+		
+		function isNotVaild(string){
+			var len = string.trim().length;
+			if(len <= 4 ||  len >=16){
+				return true;
 			}
 		}
-			
-	});
+		
+		function loginErrorMessage(message){
+			var html = $.parseHTML("<div class='alert alert-danger' role='alert'>" + message +"</div>");
+		    $("#alertTemplate").append(html);
+			setTimeout(function(){
+				$("#alertTemplate").html("");
+			}, 2000);
+		}
 	
-});
+		$("body").on("keyup", function(event){
+			if(event.keyCode === 13){
+				if(vaild_Form()){
+					login();
+				}
+			}
+				
+		});
+	});
+}(jQuery));
--- js/registration.js
+++ js/registration.js
@@ -1,93 +1,93 @@
-$(document).ready(function(){
-	
-	$("form button").click(function(e){
-		var username = $("form input[type='text']").val();
-		var password1 = $("#password1").val();
-		var password2 = $("#password2").val();
-		if(isNotVaild(username) || isNotVaild(password1) || isNotVaild(password2)){
-			return;
+(function($){
+	$(function(){
+		$("form button").click(function(e){
+			var username = $("form input[type='text']").val();
+			var password1 = $("#password1").val();
+			var password2 = $("#password2").val();
+			if(isNotVaild(username) || isNotVaild(password1) || isNotVaild(password2)){
+				return;
+			}
+			
+			if(password1 != password2){
+				return;
+			}
+			// using ajax request for login in
+			$.ajax({
+				  type: "POST",
+				  url: 'index.php/registration/register',
+				  data: {'username':username, 'password':password1},
+				  success: function(result){
+					  if(result.status){
+						  window.location.href = 'index.php/login';
+					  }else{
+						  loginErrorMessage(result.message);
+					  }
+						  
+				  },
+				  error:function(result){
+					  loginErrorMessage("用户注册时发生错误！");
+				  },
+				  dataType: 'json'
+				});
+		});
+		
+		$("form input[type='text']").blur(function(e){
+			if(isNotVaild($(this).val())){
+				$(this).parent().removeClass('has-success');
+				$(this).parent().addClass('has-error');
+			    $(this).next().text("用户名不合法，请重新输入！");
+			}else{
+				$(this).parent().removeClass('has-error');
+				$(this).parent().addClass('has-success');
+				$(this).next().text("");
+			}
+		});
+		
+		$("form input[type='password']").blur(function(e){
+			
+			if(isNotVaild($(this).val())){
+				$(this).parent().removeClass('has-success');
+				$(this).parent().addClass('has-error');
+				$(this).next().text("密码不合法，请重新输入！");
+				return;
+			}else{
+				$(this).parent().removeClass('has-error');
+				$(this).parent().addClass('has-success');
+				$(this).next().text("");
+			}
+			var pw_obj  = $("#password1");
+			var pw2_obj = $("#password2");
+			var password1 = pw_obj.val();
+			var password2 = pw2_obj.val();
+			if(password1 != password2){
+				pw_obj.parent().removeClass('has-success');
+				pw2_obj.parent().removeClass('has-success');
+				pw_obj.parent().addClass('has-error');
+				pw2_obj.parent().addClass('has-error');
+				$(this).next().text("两次密码输入不匹配！");
+			}else{
+				pw_obj.parent().removeClass('has-error');
+				pw2_obj.parent().removeClass('has-error');
+				pw_obj.parent().addClass('has-success');
+				pw2_obj.parent().addClass('has-success');
+				pw_obj.next().text("");
+				pw2_obj.next().text("");
+			}
+		});
+		
+		function isNotVaild(string){
+			var len = string.trim().length;
+			if(len <= 4 ||  len >=16){
+				return true;
+			}
 		}
 		
-		if(password1 != password2){
-			return;
-		}
-		// using ajax request for login in
-		$.ajax({
-			  type: "POST",
-			  url: 'index.php/registration/register',
-			  data: {'username':username, 'password':password1},
-			  success: function(result){
-				  if(result.status){
-					  window.location.href = 'index.php/login';
-				  }else{
-					  loginErrorMessage(result.message);
-				  }
-					  
-			  },
-			  error:function(result){
-				  loginErrorMessage("用户注册时发生错误！");
-			  },
-			  dataType: 'json'
-			});
-	});
-	
-	$("form input[type='text']").blur(function(e){
-		if(isNotVaild($(this).val())){
-			$(this).parent().removeClass('has-success');
-			$(this).parent().addClass('has-error');
-		    $(this).next().text("用户名不合法，请重新输入！");
-		}else{
-			$(this).parent().removeClass('has-error');
-			$(this).parent().addClass('has-success');
-			$(this).next().text("");
+		function loginErrorMessage(message){
+			var html = $.parseHTML("<div class='alert alert-danger' role='alert'>" + message +"</div>");
+		    $("#alertTemplate").append(html);
+			setTimeout(function(){
+				$("#alertTemplate").html("");
+			}, 2000);
 		}
 	});
-	
-	$("form input[type='password']").blur(function(e){
-		
-		if(isNotVaild($(this).val())){
-			$(this).parent().removeClass('has-success');
-			$(this).parent().addClass('has-error');
-			$(this).next().text("密码不合法，请重新输入！");
-			return;
-		}else{
-			$(this).parent().removeClass('has-error');
-			$(this).parent().addClass('has-success');
-			$(this).next().text("");
-		}
-		var pw_obj  = $("#password1");
-		var pw2_obj = $("#password2");
-		var password1 = pw_obj.val();
-		var password2 = pw2_obj.val();
-		if(password1 != password2){
-			pw_obj.parent().removeClass('has-success');
-			pw2_obj.parent().removeClass('has-success');
-			pw_obj.parent().addClass('has-error');
-			pw2_obj.parent().addClass('has-error');
-			$(this).next().text("两次密码输入不匹配！");
-		}else{
-			pw_obj.parent().removeClass('has-error');
-			pw2_obj.parent().removeClass('has-error');
-			pw_obj.parent().addClass('has-success');
-			pw2_obj.parent().addClass('has-success');
-			pw_obj.next().text("");
-			pw2_obj.next().text("");
-		}
-	});
-	
-	function isNotVaild(string){
-		var len = string.trim().length;
-		if(len <= 4 ||  len >=16){
-			return true;
-		}
-	}
-	
-	function loginErrorMessage(message){
-		var html = $.parseHTML("<div class='alert alert-danger' role='alert'>" + message +"</div>");
-	    $("#alertTemplate").append(html);
-		setTimeout(function(){
-			$("#alertTemplate").html("");
-		}, 2000);
-	}
-	
-});
+}(jQuery));
