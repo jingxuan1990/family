6c954275e9f1c2a833630961756e72698d74c8da
Andy Li <jingxuan2046@gmail.com>
2014-11-22 13:45:31 +0800

    modify migration files
    modify migration files

--- application/migrations/001_init_db.php
+++ application/migrations/001_init_db.php
@@ -12,19 +12,20 @@ class Migration_Init_db extends CI_Migration
               `log_time` varchar(45) NOT NULL,
               `money` decimal(11,2) NOT NULL,
               PRIMARY KEY (`id`)
-            ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+            ) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
        " );
         
         $this->db->query ( "
             CREATE TABLE `user` (
               `id` int(11) NOT NULL AUTO_INCREMENT,
-              `username` varchar(50) CHARACTER SET utf8 NOT NULL,
-              `password` varchar(50) CHARACTER SET utf8 NOT NULL,
-              `identity` bit(1) NOT NULL,
-              `log_time` datetime NOT NULL,
-              `count` int(11) DEFAULT NULL,
-              PRIMARY KEY (`id`)
-            ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+              `username` varchar(50) NOT NULL,
+              `password` varchar(50) NOT NULL,
+              `identity` tinyint(4) NOT NULL DEFAULT '0',
+              `log_time` datetime DEFAULT NULL,
+              `count` int(11) NOT NULL DEFAULT '0',
+              PRIMARY KEY (`id`),
+              UNIQUE KEY `username_UNIQUE` (`username`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
        " );
     }
     
